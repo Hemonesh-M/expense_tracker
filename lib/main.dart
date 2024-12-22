cc8a0f75a95878ad20e13787446e68c20a084a33
HEMONESH MAHESHWARI <143785826+Hemonesh-M@users.noreply.github.com>
2024-12-23 00:44:19 +0530

    Allowed Portrait Mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,14 @@
 import 'package:expense_tracker/theme/custom_themes.dart';
 import 'package:expense_tracker/widget/expenses.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+// import 'package:flutter/services.dart';
 
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setPreferredOrientations([
-    DeviceOrientation.portraitUp
-  ]).then((fn){
+  // WidgetsFlutterBinding.ensureInitialized();
+  // SystemChrome.setPreferredOrientations([
+  //   DeviceOrientation.portraitUp
+  // ]).then((fn){
   runApp(
     MaterialApp(
       themeMode: ThemeMode.system,
@@ -19,5 +19,5 @@ void main() {
       home: const Expenses(),
     ),
   );
-  });
+  // });
 }
